7de6ac1d70459894e9b23b19260820ac8ed5aab7
Yuki Takemoto <take5tatake@outlook.jp>
2017-01-25 22:25:08 +0900

    線を引けるようにした

--- AngryCircle/src/angrycircle/AngryCircle.java
+++ AngryCircle/src/angrycircle/AngryCircle.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Light;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -22,6 +23,11 @@ import javafx.stage.Stage;
  * @author take5
  */
 public class AngryCircle extends Application {
+
+    private double startX;
+    private double startY;
+    private double endX;
+    private double endY;
 
     @Override
     public void start(Stage primaryStage) {
@@ -34,9 +40,22 @@ public class AngryCircle extends Application {
 
         gc.setFill(Color.BLUE);
         gc.fillRect(75, 75, 100, 100);
+
+        canvas.setOnMousePressed(e -> {
+            startX = e.getX();
+            startY = e.getY();
+        });
         
-        canvas.setOnMouseClicked(e -> gc.fillRect(e.getX(), e.getY(), 10, 10));
-        
+        canvas.setOnMouseDragged(e -> {
+            endX = e.getX();
+            endY = e.getY();
+            gc.strokeLine(startX, startY, endX, endY);
+            startX = endX;
+            startY = endY;
+            
+            
+        });
+
         root.getChildren().add(canvas);
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
